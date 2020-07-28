a540c27de9487b99d71039e3a8a3eea4da3deb18
Abdulsalam Ibrahim <abdulsalamibrahim7@gmail.com>
2020-07-28 20:16:14 +0200

    Update app.j
    updated avgPace

--- apps/tracker/app.js
+++ apps/tracker/app.js
@@ -349,7 +349,7 @@ function detectActivity() {
 
 function storeFinalActivityData() {
   var avgHr = (totHr/totTime);
-  avgPace = totTime/totDist;
+  avgPace = totTime === 0 || totDist === 0 ? 0 : totTime/totDist;
   finalData = {steps: totSteps, time: totTime, distance:totDist, AverageHR: avgHr, Pace: avgPace,cadence: totCadence, activity:activity};
   finalDataString = JSON.stringify(finalData);
 }
